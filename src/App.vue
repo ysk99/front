f505c091acaf94200b73ee2f666062dde2b305a8
unknown <yanshuaike@intranet.dalianair.com>
2019-04-10 10:29:17 +0800

    第一次正式更新

--- src/App.vue
+++ src/App.vue
@@ -67,7 +67,7 @@ import { formatDate } from '@/utils/date.js'
       //   description         : '一个聚合式影视搜索网站，更新快、资源好，欢迎分享使用', // 描述, 默认读取head标签：<meta name="description" content="PHP弱类型的实现原理分析" />
       //   // image               : '', // 图片, 默认取网页中第一个img标签
       //   sites               : ['wechat', 'weibo', 'qq', 'douban'], // 启用的站点
-      //   // disabled            : [],
+      //   // disabled            : [], // 禁用的站点
       //   wechatQrcodeTitle   : '微信扫一扫：分享', // 微信二维码提示文字
       //   wechatQrcodeHelper  : '<p>微信里点“发现”，扫一下</p><p>二维码便可将本文分享至朋友圈。</p>'
       // },
